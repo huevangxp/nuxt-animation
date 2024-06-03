a5798e4f25a7c06931f66cfdaccdaa5e2f8d1c37
huevangxp <huevang933@gmail.comcd>
2024-06-03 15:28:19 +0700

    new

--- components/Product/News.vue
+++ components/Product/News.vue
@@ -3,7 +3,7 @@
      <v-row>
        <v-col cols="12" >
          <v-card elevation="0">
-           <v-img src="https://scontent.fvte2-3.fna.fbcdn.net/v/t39.30808-6/445365676_838882641603589_2838143166885396624_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=5f2048&_nc_ohc=cU53dvoJStEQ7kNvgFEsN70&_nc_ht=scontent.fvte2-3.fna&oh=00_AYAIsVWIvPP4dXnldGleTOJRYw3eYl3oZFrxC5AfJRSOMg&oe=665637CC"></v-img>
+           <v-img src="https://scontent.fbkk10-1.fna.fbcdn.net/v/t39.30808-6/444935418_845431087615411_4729601989568630131_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeGfRPsXWDlgethzn4jpnAuW6MZUrpQY_1ToxlSulBj_VKshEJQ7LCze0ST9KtXwFOVzg4RSViZ-sNKS0Uu20JcR&_nc_ohc=R_RXTe0PlSwQ7kNvgFbvpQP&_nc_ht=scontent.fbkk10-1.fna&oh=00_AYCJHpynIpRHS1iUZalB7U73MlEof5gqSyuTlPlP5O_iyQ&oe=666347FA"></v-img>
            <p class="pt-4">
              ແຈກສູດ Challenge ເກັບເງິນຕາມອາຍຸ ກ່ອນນອນ🌙ສຳລັບຄົນທີ່ຢາກສ້າງວິໄນທາງການເງິນ.🗒️
            </p>
@@ -12,9 +12,9 @@
            </p>
          </v-card>
        </v-col>
-       <v-col v-for="item in 2" cols="12" md="6">
+       <v-col v-for="item in 2" :key="item" cols="12" md="6">
          <v-card elevation="0">
-           <v-img src="https://scontent.fvte2-3.fna.fbcdn.net/v/t39.30808-6/369682653_675087441316444_1870515133416054707_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=5f2048&_nc_ohc=eNAzbyZyU7oQ7kNvgH-hsEu&_nc_ht=scontent.fvte2-3.fna&oh=00_AYAW29TXnwDb9T7U5T39ImrHsnkJ8YxmCUeiAKzfHXSIhw&oe=66564086">
+           <v-img src="https://scontent.fbkk10-1.fna.fbcdn.net/v/t39.30808-6/444935418_845431087615411_4729601989568630131_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeGfRPsXWDlgethzn4jpnAuW6MZUrpQY_1ToxlSulBj_VKshEJQ7LCze0ST9KtXwFOVzg4RSViZ-sNKS0Uu20JcR&_nc_ohc=R_RXTe0PlSwQ7kNvgFbvpQP&_nc_ht=scontent.fbkk10-1.fna&oh=00_AYCJHpynIpRHS1iUZalB7U73MlEof5gqSyuTlPlP5O_iyQ&oe=666347FA">
 
            </v-img>
            <p class="pt-4"> ໃນຕອນບ່າຍຂອງວັນພຸດ ວັນທີ 09 ມີນາ 2022 ເວລາ 14:30 ໂມງ ຢູ່ທີ່ຫ້ອງປະຊຸມຊັ້ນ 8 ຂອງ ທະນາຄານສົ່ງເສີມກະສິກຳ...</p>
